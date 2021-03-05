b38f960ee75d249c7e617d96855b2320a197c62b
Vitaly Engel <vitaliy.engel@gmail.com>
2021-03-05 13:25:06 +0300

    exspress config

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const app = express();
+
+app.get('/',(req, res) => {
+  res.send({message: "Hello world"});
+});
+
+app.listen(3333, () => {
+  console.log('Application listening on port 3333!');
+});
